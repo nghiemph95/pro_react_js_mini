d3aec193f5649eee27f70c324cba633337d21157
nghiemph95 <nghiemph95@gmail.com>
2022-03-04 23:54:55 +0700

    update

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+function Navbar() {
+  return (
+    <NavbarContainer>
+      <Text></Text>
+    </NavbarContainer>
+  );
+}
+
+const NavbarContainer = styled.nav``;
+
+const Text = styled.h1``;
