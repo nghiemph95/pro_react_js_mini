8c2f46b97e66c76f4b751532a2459f3b59b04d70
nghiemph95 <nghiemph95@gmail.com>
2022-03-05 23:01:13 +0700

    05/03/2022

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
+import MainContent from "./components/MainContent";
 
 function App() {
   return (
     <Container>
       <Sidebar />
-      {/* <MainContent/> */}
+      <MainContent/>
     </Container>
   );
 }
